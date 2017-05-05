7d9b90fcb76b6860664774ec3ae1551f6c227be6
hxl-dy <hexulei@dyinnovations.com>
2017-05-05 13:39:04 +0800

    update to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,78 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const minimize = process.argv.indexOf('--no-minimize') === -1;
+const minimize = process.argv.indexOf('--optimize-minimize') === -1;
 const plugin = [];
 let filename = 'rtmp.js';
 
 if (minimize) {
-  plugin.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+  plugin.push(new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    sourceMap: true,
+  }));
   filename = 'rtmp.min.js';
 }
 
 module.exports = {
-  entry: path.resolve(__dirname, 'index.js'),
+  entry: './index.js',
   devtool: minimize ? 'source-map' : '',
   externals: {
     clappr: 'Clappr',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel',
-        query: {
-          compact: true,
-        },
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              compact: false,
+            },
+          },
+        ],
       },
       {
         test: /\.scss$/,
-        loaders: ['css', `sass?includePaths[]=${
-                   path.resolve(__dirname, './node_modules/compass-mixins/lib')
-                   }&includePaths[]=${
-                   path.resolve(__dirname, './node_modules/clappr/src/base/scss')
-                   }&includePaths[]=${
-                   path.resolve(__dirname, './src/base/scss')}`,
+        use: [
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                path.resolve(__dirname, './node_modules/compass-mixins/lib'),
+                path.resolve(__dirname, './node_modules/clappr/src/base/scss'),
+                path.resolve(__dirname, './src/base/scss'),
+              ],
+            },
+          },
         ],
-        include: path.resolve(__dirname, 'src'),
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
       },
       {
-        test: /\.html/, loader: 'html?minimize=false',
+        test: /\.html/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+            },
+          },
+        ],
       },
       {
-        test: /\.(png|woff|eot|ttf|swf)/, loader: 'file-loader',
+        test: /\.(png|woff|eot|ttf|swf)/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ],
-  },
-  resolve: {
-    extensions: ['', '.js'],
   },
   plugins: plugin,
   output: {
